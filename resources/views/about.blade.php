47efac68e7e781c8b145b49e1907aac6fb8dd6aa
Ezequiel Muniz <muniz2405@gmail.com>
2024-10-21 19:48:28 -0300

    update mensagens

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,8 @@
 <x-message-banner msg="user signup has been done" class="success"/>
+<x-message-banner msg="Server is dow" class="error"/>
+<x-message-banner msg="No product found" class="warning"/>
 <h1>
-    Esta página é sobre a Fabiana
+    About page
 </h1>
 @include('common.inner', ["page"=>"this is abaut page"])
 
@@ -10,6 +12,22 @@
         color:green;
         padding:3px 10 px;
         margin: 10 px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .error{
+        background-color:yellow;
+        color:red;
+        padding:3px 10px;
+        margin: 10px;
+        display: inline-block;
+        border-radius: 2px;
+    }
+    .warning{
+        background-color:#ffa50094;
+        color:orange;
+        padding:3px 10px;
+        margin: 10px;
         display: inline-block;
         border-radius: 2px;
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,24 +11,24 @@
     .success{
         background-color:lightgreen;
         color:green;
-        padding:3px 10 px;
-        margin: 10 px;
+        padding:3px 10px;
+        margin: 10px;
         display: inline-block;
         border-radius: 2px;
     }
     .error{
         background-color:yellow;
         color:red;
-        padding:3px 10 px;
-        margin: 10 px;
+        padding:3px 10px;
+        margin: 10px;
         display: inline-block;
         border-radius: 2px;
     }
     .warning{
         background-color:#ffa50094;
         color:orange;
-        padding:3px 10 px;
-        margin: 10 px;
+        padding:3px 10px;
+        margin: 10px;
         display: inline-block;
         border-radius: 2px;
     }
